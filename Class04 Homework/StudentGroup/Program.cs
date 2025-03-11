750728b9196badd5761aef5d4cddc4eb80902028
MartinPopovski95 <popovskimartin1995@gmail.com>
2025-03-11 19:43:00 +0100

    Class04 Homework

---
+++ Class04 Homework/StudentGroup/Program.cs
@@ -0,0 +1,53 @@
+﻿//## Task 2
+//using static System.Net.Mime.MediaTypeNames;
+//using System.Diagnostics.Metrics;
+
+//*Make a new console application called StudentGroup
+//* Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names. 
+//* Get a number from the console between 1 and 2 and print the students from that group in the console.
+//* Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+//* Test Data:
+//*Enter student group: (there are 1 and 2 )
+//    * 1
+//* Expected Output:
+//  *The Students in G1 are: 
+//  *Zdravko
+//  * Petko
+//  * Stanko
+//  * Branko
+//  * Trajko
+
+
+string[] studentsG1 = new string[] {"Dona", "Martin", "Aston", "Badi", "Nero" };
+string[] studentsG2 = new string[] { "Eva", "Zoran", "Oliver", "Beti", "Gordana" };
+
+Console.WriteLine("Enter student group (1 or 2):");
+bool success = int.TryParse(Console.ReadLine(), out int userInput);
+
+if (success)
+{
+    switch (userInput)
+    {
+        case 1:
+            Console.WriteLine("The Students in G1 are:");
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+                break;
+        case 2:
+            Console.WriteLine("The Students in G2 are:");
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+                break;
+        default:
+            Console.WriteLine("Invalid input!");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input! Please try again");
+}
---
+++ Class04 Homework/SumOfEven/Program.cs
@@ -0,0 +1,55 @@
+﻿//## Task 1
+//using static System.Net.Mime.MediaTypeNames;
+//using System.Diagnostics.Metrics;
+
+//Make a console application called 
+//4. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+//*Test Data:
+//*Enter integer no.1:
+//    *4
+//  * Enter integer no.2:
+//    *3
+//  * Enter integer no.3:
+//    *7
+//  * Enter integer no.4:
+//    *3
+//  * Enter integer no.5:
+//    *2
+//  * Enter integer no.6:
+//    *8
+//* Expected Output:
+//*The result is: 14
+
+int[] arrayOfInts = new int[6];
+
+int promptCounter = 1;
+int result = 0;
+
+for(int i = 0; i < arrayOfInts.Length; i++)
+{
+    Console.WriteLine($"Enter intiger no.{promptCounter++}:");
+    bool success = int.TryParse(Console.ReadLine(), out int numberInput);
+
+    if (success)
+    {
+        arrayOfInts[i] = numberInput;
+
+        if (numberInput % 2 == 0)
+        {
+            result += numberInput;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input! Please enter a valid number");
+        i--;
+        promptCounter--;
+    }
+
+}
+
+Console.WriteLine($"The result is: {result}");
